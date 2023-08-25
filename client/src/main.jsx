be3c1c247d62016e40f97cc9b91921b06ba4d864
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-08-24 21:10:19 -0400

    making the main pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,45 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+
+import Login from './pages/Login';
+
+import Success from './pages/Success';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    error: <NoMatch />,
+    children: [
+      {
+        index: true, 
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/success',
+        element: <Success />
+      }, {
+        path: '/orderHistory',
+        element: <OrderHistory />
+      }, {
+        path: '/products/:id',
+        element: <Detail />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<App />
+  <RouterProvider router={router} />
 )
