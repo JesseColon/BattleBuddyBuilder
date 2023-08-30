ed3128d6a7e3aa855c34bd439d3f9dabff92e9aa
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-08-30 16:39:47 -0400

    deteled the pages not needed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,12 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
 import Home from './pages/Home';
-import Detail from './pages/Detail';
+// import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Success from './pages/Success';
-import OrderHistory from './pages/OrderHistory';
+// import Success from './pages/Success';
+// import OrderHistory from './pages/OrderHistory';
 import Pikachu from './pages/Pikachu';
 import Teams from  './pages/Teams';
 import PokemonTeamB from './pages/PokemonTeamB';
@@ -31,10 +31,12 @@ const router = createBrowserRouter([
       }, {
         path: '/signup',
         element: <Signup />
-      }, {
-        path: '/success',
-        element: <Success />
-      },{
+      }, 
+      // {
+      //   path: '/success',
+      //   element: <Success />
+      // },
+      {
         path: '/pikachu',
         element: <Pikachu />
       },{
@@ -50,13 +52,14 @@ const router = createBrowserRouter([
         path: '/pokemonCard',
         element: <PokemonCard/>
       },
-       {
-        path: '/orderHistory',
-        element: <OrderHistory />
-      }, {
-        path: '/products/:id',
-        element: <Detail />
-      }
+      //  {
+      //   path: '/orderHistory',
+      //   element: <OrderHistory />
+      // }, 
+      // {
+      //   path: '/products/:id',
+      //   element: <Detail />
+      // }
     ]
   }
 ]);
