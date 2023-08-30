fefef1d2230d54de3b92a4d44aceca8880615818
osiris <king.forrest828@gmail.com>
2023-08-29 20:08:35 -0400

    Merge and some updates

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,9 @@
 const User = require('./User');
+const Move = require('./Move');
+const Pokemon = require('./Pokemon');
+const Team = require('./Team');
 
 module.exports = { User };
+module.exports = { Move };
+module.exports = { Pokemon };
+module.exports = { Team };
